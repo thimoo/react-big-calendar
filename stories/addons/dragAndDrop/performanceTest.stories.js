ce23517499dddd13eebb297991fccb8848124ef5
Billy Utermann <billy@thimoo.ch>
2023-03-01 09:34:39 +0100

    docs: add perftest

--- stories/addons/dragAndDrop/performanceTest.stories.js
+++ stories/addons/dragAndDrop/performanceTest.stories.js
@@ -1,18 +1,40 @@
 import React, { Profiler } from 'react'
-import { Calendar, dayjsLocalizer, momentLocalizer } from '../../../src'
+import {
+  Calendar,
+  dayjsLocalizer,
+  momentLocalizer,
+  dateFnsLocalizer,
+} from '../../../src'
 import withDragAndDrop from '../../../src/addons/dragAndDrop'
 import moment from 'moment'
 //import 'moment-timezone'
 //moment.tz.setDefault('America/New_York')
-const localizer = momentLocalizer(moment)
+//const localizer = momentLocalizer(moment)
 
 const DnDCalendar = withDragAndDrop(Calendar)
 
 import dayjs from 'dayjs'
-import timezone from 'dayjs/plugin/timezone'
-dayjs.extend(timezone)
-dayjs.tz.setDefault('America/New_York')
-// const localizer = dayjsLocalizer(dayjs)
+//import timezone from 'dayjs/plugin/timezone'
+//dayjs.extend(timezone)
+//dayjs.tz.setDefault('America/New_York')
+//const localizer = dayjsLocalizer(dayjs)
+
+import format from 'date-fns/format'
+import startOfWeek from 'date-fns/startOfWeek'
+import getDay from 'date-fns/getDay'
+import frCH from 'date-fns/locale/fr-CH'
+
+const locales = {
+  'fr-CH': frCH,
+}
+
+const localizer = dateFnsLocalizer({
+  format,
+  startOfWeek,
+  getDay,
+  locales,
+  firstDayOfWeek: 7,
+})
 
 export default {
   title: 'Addons/Drag and Drop',
@@ -27,9 +49,9 @@ export default {
 const events = []
 
 const eventLengthInSec = 60
-const numEvents = 500
+const numEvents = 10080
 
-for (let i = 1; i < 10080; i++) {
+for (let i = 1; i < numEvents; i++) {
   events.push({
     title: `e${i}`,
     id: i,
@@ -51,7 +73,8 @@ export function PerformanceTest() {
         id="Calendar"
         onRender={(...c) => console.log('render Duration', c[1], c[2])}
       >
-        <DnDCalendar
+        {/* DnDCalendar?*/}
+        <Calendar
           dayLayoutAlgorithm="no-overlap"
           events={events}
           localizer={localizer}
